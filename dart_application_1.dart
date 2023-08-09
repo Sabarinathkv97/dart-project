2f0cd11d0bb8c5c39c06db97d840b89cf646426f
Sabarinath K V <141605987+Sabarinathkv97@users.noreply.github.com>
2023-08-09 11:03:25 +0530

    Add files via upload

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -1,15 +1,3 @@
 void main() {
-  int reminder, sum = 0, temp;
-  int number = 8989;
-  temp = number;
-  while (number > 0) {
-    reminder = number % 10;
-    sum = (sum * 10) + reminder;
-    number = number ~/ 10;
-  }
-  if (sum == temp) {
-    print('Its A Palindrome number');
-  } else {
-    print('Its A Not Palindrome number');
-  }
+  print("hello world");
 }
